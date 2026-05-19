e2a0c649291046435c0c005c3ad2796a9c3f50eb
Shahzaib Saeed <shahzaibsaeed077@gmail.com>
2026-05-19 17:33:04 +0500

    fix added

--- app.js
+++ app.js
@@ -27,17 +27,17 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow Postman / curl
+    if (!origin) return callback(null, true);
 
     if (allowedOrigins.includes(origin)) {
-      return callback(null, origin); // dynamic origin
+      return callback(null, origin);
     } else {
       return callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: '*'
 }));
 
 /**
